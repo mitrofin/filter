fcc7b2f4d87928a9064fc0343159554433dda7d5
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-08-24 16:17:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import User from "./User";
 
 import data from "./MOCK_DATA.json";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [users, setUsers] = useState(data);
+
+  const filterUser = (searchText, usersList) => {
+    if (!searchText) {
+      return usersList;
+    }
+    return usersList.filter(
+      ({ first_name, car }) =>
+        first_name.toLowerCase().includes(searchText.toLowerCase()) ||
+        car.toLowerCase().includes(searchText.toLowerCase())
+    );
+  };
+  useEffect(() => {
+    const Debounce = setTimeout(() => {
+      const filteredUsers = filterUser(searchTerm, data);
+      setUsers(filteredUsers);
+    }, 300);
+    return () => clearTimeout(Debounce);
+  }, [searchTerm]);
+
   return (
     <>
       <div className="App">
@@ -26,7 +46,14 @@ function App() {
               {users.map((user, index) => {
                 return (
                   <li key={index} className="user">
-                    {user.city}
+                    <User
+                      key={index}
+                      f_name={user.first_name}
+                      l_name={user.last_name}
+                      cit={user.city}
+                      ca={user.car}
+                      gen={user.gender}
+                    />
                   </li>
                 );
               })}
